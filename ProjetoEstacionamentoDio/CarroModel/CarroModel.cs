2b73fd12a2725ffa72b5255075438e4e6219b41c
Luanf38 <luanfelipe38@hotmail.com>
2024-10-10 20:38:50 -0300

    first commit

---
+++ ProjetoEstacionamentoDio/CarroModel/CarroModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoEstacionamentoDio
+{
+    internal class CarroModel
+    {
+        string ModeloCarro { get; set; }
+        string PlacaCarro { get; set; }
+    }
+}
---
+++ ProjetoEstacionamentoDio/Estacionamento/Estacionamento.cs
@@ -0,0 +1,27 @@
+﻿
+using ProjetoEstacionamentoDio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoEstacionamentoDio.Estacionamento
+{
+    internal class Estacionamento
+    {
+        List<CarroModel> carroModels = [];
+        
+        public void adicionarCarro(CarroModel carro)
+        {
+            carroModels.Add(carro);
+            Console.WriteLine("Carro Adicionado Com Sucesso");
+        }
+
+        public void removerCarro(CarroModel carro)
+        {
+            carroModels.Remove(carro);
+            Console.WriteLine("Carro Removido");
+        }
+    }
+}
